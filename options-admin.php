27ab5f3fc744030f6572a708d4170700071a8723
Michael McNeill <michael@michaelryanmcneill.com>
2018-05-16 01:37:39 -0400

    Touch ups for attribute_access_custom

--- options-admin.php
+++ options-admin.php
@@ -295,7 +295,7 @@ function shibboleth_options_page() {
 						. 'your Shibboleth Service Provider on a reverse proxy, you should select the "HTTP Headers" option and, if at all possible, add a spoofkey below.', 'shibboleth'); ?></p>
 					</td>
 				</tr>
-				<tr id="attribute_access_custom_row" <?php ( $attribute_access_custom === true ?: echo 'style="display:none;"'; ) ?>>
+				<tr id="attribute_access_custom_row" <?php ( $attribute_access_custom === true ?: echo 'style="display:none;"' ); ?>>
 					<th scope="row"><label for="attribute_access_custom"><?php _e('Custom Attribute Access Prefix', 'shibboleth'); ?></label></th>
 					<td>
 						<input type="text" id="attribute_access_custom" name="attribute_access_custom" value="<?php echo $attribute_access; ?>" size="50" <?php if ( defined( 'SHIBBOLETH_ATTRIBUTE_ACCESS_METHOD' ) ) { disabled( $attribute_access, SHIBBOLETH_ATTRIBUTE_ACCESS_METHOD ); } ?> /><br />
