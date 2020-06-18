beeda9f7b31a6c6bbb51b9c112e5f4fa1b3cf42c
Michael McNeill <michael@michaelryanmcneill.com>
2020-06-18 16:25:26 -0400

    Failed to update version in shibboleth.php

--- shibboleth.php
+++ shibboleth.php
@@ -4,7 +4,7 @@
  Plugin URI: http://wordpress.org/extend/plugins/shibboleth
  Description: Easily externalize user authentication to a <a href="http://shibboleth.internet2.edu">Shibboleth</a> Service Provider
  Author: Michael McNeill, mitcho (Michael 芳貴 Erlewine), Will Norris
- Version: 2.2
+ Version: 2.2.1
  License: Apache 2 (http://www.apache.org/licenses/LICENSE-2.0.html)
  Text Domain: shibboleth
  */
